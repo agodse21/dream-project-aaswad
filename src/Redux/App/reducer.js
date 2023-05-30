0da3c686de211b7e26fd63c897a02cb28a62c9b0
agodse21 <agodse9@gmail.com>
2023-05-30 22:34:10 +0530

    feat(redux): :zap: Redux added and updated react app

---
+++ src/Redux/App/reducer.js
@@ -0,0 +1,3 @@
+export const reducer = () => {
+  return <div>reducer</div>;
+};
--- src/Redux/rootReducer.js
+++ src/Redux/rootReducer.js
@@ -1,14 +1,9 @@
 import { combineReducers } from "redux";
-import { reducers as AppReducer } from "./App/reducer";
-import { reducer as CartReducer } from "./Addtocart/reducer";
-import { reducer as WishlistReducer } from "./wishlist/reducer";
-import { AuthReducer } from "./Auth/reducer";
-
+import { reducer as AuthReducer } from "./Auth/reducer";
+import { reducer as AppReducer } from "./App/reducer";
 const rootReducer = combineReducers({
   AppReducer,
-  CartReducer,
   AuthReducer,
-  WishlistReducer,
 });
 
 export default rootReducer;
